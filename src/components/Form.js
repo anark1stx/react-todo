34f1b8e208b622880421f8064091b976720d783f
Mateo Carriqui <mateocarriqui7@gmail.com>
2020-12-29 01:12:28 -0300

    fixed id genration function to disable a warning.

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,9 +20,9 @@ const Form = (props) => {
     props.setStatus(e.target.value);
   };
 
-  function uuidv4() { //thanks to: https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid/2117523#2117523
+  function uuidv4() { //thanks to: https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid/2117523#2117523 , replaced c=='x' with c==='x' and r & 0x3 with r && 0x3 to disable a warning
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r && 0x3 | 0x8);
       return v.toString(16);
     });
   }
